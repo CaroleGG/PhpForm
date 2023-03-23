65ce7ba08b46551e0060669e656a78b58ee1924e
CaroleGG <[caroguerite@hotmail.com](mailto:caroguerite@hotmail.com)>
2023-03-23 22:37:13 +0100

    nettoyage fichier index.php

--- index.php
+++ index.php
@@ -19,42 +19,7 @@
     
         <?php include "form.php" ;?>
    
-<!--
-    <form action="thanks.php" method="post">
-        <div>
-            <label for="name">Nom :</label>
-            <input type="text" id="name" name="user_name">
-        </div>
-        <div>
-            <label for="firstname">Prénom :</label>
-            <input type="text" id="firstname" name="user_firstname">
-        </div>
-        <div>
-            <label for="courriel">Courriel :</label>
-            <input type="email" id="courriel" name="user_email">
-        </div>
-        <div>
-            <label for="phoneNumber">Téléphone :</label>
-            <input type="text" id="phoneNumber" name="user_phone">
-        </div>
-        <div>
-            <label for="about">Sujet :</label>
-            <select id="about" name="user_about">
-                <option value="item0">Choose:</option>
-                <option value="item1">Item_1</option>
-                <option value="item2">Item_2</option>
-                <option value="item3">Item_3</option>
-                <option value="item4">Item_4</option>
-            </select>
-        </div>
-        <div>
-            <label for="message">Message :</label>
-            <textarea id="message" name="user_message"></textarea>
-        </div>
-        <div class="button">
-            <button type="submit">Envoyer votre message</button>
-        </div>
--->
+
         <!-- MDB -->
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
 </body>
